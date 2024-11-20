42c9a8a4fdbbbdb74e5389fef61bb1f0364909c2
Oystein Fladby <ofladby@hotmail.com>
2024-11-20 14:27:38 +0100

    C

---
+++ testing_B/testB/function.py
@@ -0,0 +1,21 @@
+import logging
+
+log = logging.getLogger("Brain")
+log_handler = logging.StreamHandler()
+log_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+log_handler.setFormatter(log_format)
+log.addHandler(log_handler)
+log.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    log.info( event )
+    log.info( context )
+    log.info("test")
+    return {
+        'statusCode': 200,
+        'body': "OK",
+        'headers': {
+            'Content-Type': 'application/json',
+        }
+    }
+
